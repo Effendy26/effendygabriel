b6548c815e6e5f541750d63e0d94728d84776b0a
Effendy26 <effendygabriel2006@gmail.com>
2023-10-20 08:09:17 +0700

    Mengubah Isianya

--- dataguru.php
+++ dataguru.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 Disini adalah halaman untuk menampilkan data guru dengan menggunakan table 
 table data guru terdiri dari 
 -ID
@@ -8,3 +16,6 @@ table data guru terdiri dari
 -Action: 
  -Edit
  -Delete
+sdadsad
+</body>
+</html>
